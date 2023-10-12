e5a4e49e14a77c75df34015ebe42b80ac2d8e3c0
Clay Chipps <clay.chipps@steampunk.com>
2023-10-12 15:31:34 -0400

    feat: finish initial impl

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,55 @@
 import * as core from '@actions/core'
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from 'fs'
+import * as path from 'path'
+import {globSync} from 'glob'
+import * as xml2js from 'xml2js'
 
-async function run(): Promise<void> {
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
   try {
-    const apiVersion = core.getInput('api-version');
+    const apiVersion = core.getInput('api-version')
 
-    if (!fs.existsSync())
+    const metadataXmlFiles = globSync('./**/*-meta.xml')
 
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    for (const metadataXmlFile of metadataXmlFiles) {
+      const filepath = path.normalize(path.join(process.cwd(), metadataXmlFile))
+      fs.readFile(metadataXmlFile, (readError, data) => {
+        if (readError) {
+          throw new Error(readError?.message)
+        }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        const parser = new xml2js.Parser({explicitArray: false})
+        parser.parseString(data, (parseError, xmlJson) => {
+          if (parseError) {
+            throw new Error(parseError?.message)
+          }
 
-    core.setOutput('time', new Date().toTimeString())
+          if (xmlJson.ApexClass) {
+            xmlJson.ApexClass.apiVersion = `${apiVersion}.0`
+          } else if (xmlJson.LightningComponentBundle) {
+            xmlJson.LightningComponentBundle.apiVersion = `${apiVersion}.0`
+          } else if (xmlJson.AuraDefinitionBundle) {
+            xmlJson.AuraDefinitionBundle.apiVersion = `${apiVersion}.0`
+          }
+
+          fs.writeFile(
+            filepath,
+            new xml2js.Builder().buildObject(xmlJson),
+            writeError => {
+              if (writeError) {
+                throw new Error(writeError?.message)
+              }
+            }
+          )
+        })
+      })
+    }
   } catch (error) {
     if (error instanceof Error) {
-      core.setFailed(error.message);
+      core.setFailed(error.message)
+    }
   }
 }
-
-run()
